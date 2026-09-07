76d41ed64e4b9acbe72568e096c21e87d8e1d2fa
engaymanelhware31-lgtm <engaymanelhware31@gmail.com>
2026-09-07 08:26:36 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,87 +1,130 @@
-const CACHE_NAME = "zekr-v4";
+const CACHE_NAME = "zekr-v5";
 
 const FILES_TO_CACHE = [
+
     "/zekr/",
+
     "/zekr/index.html",
+
     "/zekr/manifest.json",
+
     "/zekr/icon-192.png",
-    "/zekr/icon-512.png"
+
+    "/zekr/icon-512.png",
+
+    "/zekr/background.png"
+
 ];
 
-self.addEventListener("install", event => {
 
-    self.skipWaiting();
+self.addEventListener(
+    "install",
+    event => {
 
-    event.waitUntil(
+        self.skipWaiting();
 
-        caches.open(CACHE_NAME).then(cache => {
+        event.waitUntil(
 
-            return cache.addAll(FILES_TO_CACHE);
+            caches.open(CACHE_NAME)
 
-        })
+                .then(cache => {
 
-    );
-
-});
-
-
-self.addEventListener("activate", event => {
-
-    event.waitUntil(
-
-        caches.keys().then(cacheNames => {
-
-            return Promise.all(
-
-                cacheNames.map(cacheName => {
-
-                    if (cacheName !== CACHE_NAME) {
-
-                        return caches.delete(cacheName);
-
-                    }
+                    return cache.addAll(
+                        FILES_TO_CACHE
+                    );
 
                 })
 
-            );
+        );
 
-        }).then(() => {
-
-            return self.clients.claim();
-
-        })
-
-    );
-
-});
+    }
+);
 
 
-self.addEventListener("fetch", event => {
+self.addEventListener(
+    "activate",
+    event => {
 
-    event.respondWith(
+        event.waitUntil(
 
-        fetch(event.request)
+            caches.keys()
 
-            .then(response => {
+                .then(cacheNames => {
 
-                const responseClone = response.clone();
+                    return Promise.all(
 
-                caches.open(CACHE_NAME).then(cache => {
+                        cacheNames.map(
+                            cacheName => {
 
-                    cache.put(event.request, responseClone);
+                                if (
+                                    cacheName !==
+                                    CACHE_NAME
+                                ) {
 
-                });
+                                    return caches.delete(
+                                        cacheName
+                                    );
 
-                return response;
+                                }
 
-            })
+                            }
+                        )
 
-            .catch(() => {
+                    );
 
-                return caches.match(event.request);
+                })
 
-            })
+                .then(() => {
 
-    );
+                    return self.clients.claim();
 
-});
+                })
+
+        );
+
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        event.respondWith(
+
+            fetch(event.request)
+
+                .then(response => {
+
+                    const copy =
+                        response.clone();
+
+
+                    caches.open(CACHE_NAME)
+
+                        .then(cache => {
+
+                            cache.put(
+                                event.request,
+                                copy
+                            );
+
+                        });
+
+
+                    return response;
+
+                })
+
+                .catch(() => {
+
+                    return caches.match(
+                        event.request
+                    );
+
+                })
+
+        );
+
+    }
+);
