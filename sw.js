94a5109fb8972bf4a34ce02dde709654546c8e7a
engaymanelhware31-lgtm <engaymanelhware31@gmail.com>
2026-09-08 10:05:41 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,93 +14,62 @@ self.addEventListener("install", event => {
     self.skipWaiting();
 
     event.waitUntil(
-
         caches.open(CACHE_NAME)
-            .then(cache => {
-
-                return cache.addAll(
-                    FILES_TO_CACHE
-                );
-
-            })
-
+            .then(cache => cache.addAll(FILES_TO_CACHE))
     );
 
 });
-
 
 self.addEventListener("activate", event => {
 
     event.waitUntil(
 
-        caches.keys()
-            .then(cacheNames => {
+        caches.keys().then(cacheNames => {
 
-                return Promise.all(
+            return Promise.all(
 
-                    cacheNames.map(
-                        cacheName => {
+                cacheNames.map(cacheName => {
 
-                            if (
-                                cacheName !==
-                                CACHE_NAME
-                            ) {
+                    if (cacheName !== CACHE_NAME) {
 
-                                return caches.delete(
-                                    cacheName
-                                );
+                        return caches.delete(cacheName);
 
-                            }
+                    }
 
-                        }
-                    )
+                })
 
-                );
+            );
 
-            })
-            .then(() => {
+        }).then(() => {
 
-                return self.clients.claim();
+            return self.clients.claim();
 
-            })
+        })
 
     );
 
 });
-
 
 self.addEventListener("fetch", event => {
 
     event.respondWith(
 
         fetch(event.request)
-
             .then(response => {
 
-                const copy =
-                    response.clone();
+                const copy = response.clone();
 
-                caches.open(
-                    CACHE_NAME
-                )
-                .then(cache => {
-
-                    cache.put(
-                        event.request,
-                        copy
-                    );
-
-                });
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(event.request, copy);
+                    });
 
                 return response;
 
             })
-
             .catch(() => {
 
-                return caches.match(
-                    event.request
-                );
+                return caches.match(event.request);
 
             })
 
