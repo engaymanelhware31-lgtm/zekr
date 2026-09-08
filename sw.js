f9e7112f5ca204ab132cb230946b4226af615b2c
engaymanelhware31-lgtm <engaymanelhware31@gmail.com>
2026-09-08 08:30:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,130 +1,109 @@
-const CACHE_NAME = "zekr-v5";
+const CACHE_NAME = "zekr-v6";
 
 const FILES_TO_CACHE = [
-
     "/zekr/",
-
     "/zekr/index.html",
-
     "/zekr/manifest.json",
-
     "/zekr/icon-192.png",
-
     "/zekr/icon-512.png",
-
     "/zekr/background.png"
-
 ];
 
+self.addEventListener("install", event => {
 
-self.addEventListener(
-    "install",
-    event => {
+    self.skipWaiting();
 
-        self.skipWaiting();
+    event.waitUntil(
 
-        event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
 
-            caches.open(CACHE_NAME)
+                return cache.addAll(
+                    FILES_TO_CACHE
+                );
 
-                .then(cache => {
+            })
 
-                    return cache.addAll(
-                        FILES_TO_CACHE
-                    );
+    );
 
-                })
-
-        );
-
-    }
-);
+});
 
 
-self.addEventListener(
-    "activate",
-    event => {
+self.addEventListener("activate", event => {
 
-        event.waitUntil(
+    event.waitUntil(
 
-            caches.keys()
+        caches.keys()
+            .then(cacheNames => {
 
-                .then(cacheNames => {
+                return Promise.all(
 
-                    return Promise.all(
+                    cacheNames.map(
+                        cacheName => {
 
-                        cacheNames.map(
-                            cacheName => {
+                            if (
+                                cacheName !==
+                                CACHE_NAME
+                            ) {
 
-                                if (
-                                    cacheName !==
-                                    CACHE_NAME
-                                ) {
-
-                                    return caches.delete(
-                                        cacheName
-                                    );
-
-                                }
+                                return caches.delete(
+                                    cacheName
+                                );
 
                             }
-                        )
 
+                        }
+                    )
+
+                );
+
+            })
+            .then(() => {
+
+                return self.clients.claim();
+
+            })
+
+    );
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        fetch(event.request)
+
+            .then(response => {
+
+                const copy =
+                    response.clone();
+
+                caches.open(
+                    CACHE_NAME
+                )
+                .then(cache => {
+
+                    cache.put(
+                        event.request,
+                        copy
                     );
 
-                })
+                });
 
-                .then(() => {
+                return response;
 
-                    return self.clients.claim();
+            })
 
-                })
+            .catch(() => {
 
-        );
+                return caches.match(
+                    event.request
+                );
 
-    }
-);
+            })
 
+    );
 
-self.addEventListener(
-    "fetch",
-    event => {
-
-        event.respondWith(
-
-            fetch(event.request)
-
-                .then(response => {
-
-                    const copy =
-                        response.clone();
-
-
-                    caches.open(CACHE_NAME)
-
-                        .then(cache => {
-
-                            cache.put(
-                                event.request,
-                                copy
-                            );
-
-                        });
-
-
-                    return response;
-
-                })
-
-                .catch(() => {
-
-                    return caches.match(
-                        event.request
-                    );
-
-                })
-
-        );
-
-    }
-);
+});
